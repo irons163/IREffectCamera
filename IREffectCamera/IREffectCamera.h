6e231a850cfd612161760ef538157e22662fe9a5
Your Name <you@example.com>
2021-04-21 12:00:44 +0800

    Initial Commit

---
+++ IREffectCamera/IREffectCamera.h
@@ -0,0 +1,18 @@
+//
+//  IREffectCamera.h
+//  IREffectCamera
+//
+//  Created by irons on 2021/4/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for IREffectCamera.
+FOUNDATION_EXPORT double IREffectCameraVersionNumber;
+
+//! Project version string for IREffectCamera.
+FOUNDATION_EXPORT const unsigned char IREffectCameraVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <IREffectCamera/PublicHeader.h>
+
+
